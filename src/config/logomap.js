ff1062fca9ec02ff99521f312905f7de97f50d36
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-14 01:07:59 +0530

    adding config files

---
+++ src/config/logomap.js
@@ -0,0 +1,12 @@
+const TEAM_LOGO = {
+  "CSK-LOGO": require("../assets/csk-logo.png"),
+  "DC-LOGO": require("../assets/dc-logo.png"),
+  "KXIP-LOGO": require("../assets/kxip-logo.png"),
+  "KKR-LOGO": require("../assets/kkr-logo.png"),
+  "MI-LOGO": require("../assets/mi-logo.png"),
+  "RR-LOGO": require("../assets/rr-logo.png"),
+  "RCB-LOGO": require("../assets/rcb-logo.png"),
+  "SRH-LOGO": require("../assets/srh-logo.png"),
+};
+Object.freeze(TEAM_LOGO);
+export default TEAM_LOGO;
---
+++ src/config/urlMap.js
@@ -0,0 +1,17 @@
+const URL = {
+  default: "https://ipl-t20.herokuapp.com/teams",
+  "chennai-super-kings":
+    "https://ipl-t20.herokuapp.com/teams/chennai-super-kings",
+  "delhi-capitals": "https://ipl-t20.herokuapp.com/teams/delhi-capitals",
+  "mumbai-indians": "https://ipl-t20.herokuapp.com/teams/mumbai-indians",
+  "rajasthan-royals": "https://ipl-t20.herokuapp.com/teams/rajasthan-royals",
+  "kings-xi-punjab": "https://ipl-t20.herokuapp.com/teams/kings-xi-punjab",
+  "royal-challengers-bangalore":
+    "https://ipl-t20.herokuapp.com/teams/royal-challengers-bangalore",
+  "sunrisers-hyderabad":
+    "https://ipl-t20.herokuapp.com/teams/sunrisers-hyderabad",
+  "kolkata-knight-riders":
+    "https://ipl-t20.herokuapp.com/teams/kolkata-knight-riders",
+};
+Object.freeze(URL);
+export default URL;
