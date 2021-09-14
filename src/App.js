d2fe91f3277aca191a2d35801b411a0d2bfac277
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-14 13:33:26 +0530

    adding Allteam Card Details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import CardContainer from "./Pages/CardContainer";
+import CardContainerAllTeams from "./Pages/CardContainerAllTeams";
 import URL from "./config/urlMap";
 import "./App.css";
+import CardContainerTeamDetails from "./Pages/CardContainerTeamDetails";
 
 function App() {
   return (
@@ -12,34 +13,33 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact>
-            <CardContainer url={URL.default} />
+            <CardContainerAllTeams />
           </Route>
           <Route path="/teams/chennai-super-kings" exact>
-            <CardContainer url={URL["chennai-super-kings"]} />
+            <CardContainerTeamDetails url={URL["chennai-super-kings"]} />
           </Route>
           <Route path="/teams/delhi-capitals" exact>
-            <CardContainer url={URL["delhi-capitals"]} />
+            <CardContainerTeamDetails url={URL["delhi-capitals"]} />
           </Route>
           <Route path="/teams/kings-xi-punjab" exact>
-            <CardContainer url={URL["kings-xi-punjab"]} />
+            <CardContainerTeamDetails url={URL["kings-xi-punjab"]} />
           </Route>
           <Route path="/teams/kolkata-knight-riders" exact>
-            <CardContainer url={URL["kolkata-knight-riders"]} />
+            <CardContainerTeamDetails url={URL["kolkata-knight-riders"]} />
           </Route>
           <Route path="/teams/mumbai-indians" exact>
-            <CardContainer url={URL["mumbai-indians"]} />
+            <CardContainerTeamDetails url={URL["mumbai-indians"]} />
           </Route>
           <Route path="/teams/rajasthan-royals" exact>
-            <CardContainer url={URL["rajasthan-royals"]} />
+            <CardContainerTeamDetails url={URL["rajasthan-royals"]} />
           </Route>
           <Route path="/teams/royal-challengers-bangalore" exact>
-            <CardContainer url={URL["royal-challengers-bangalore"]} />
+            <CardContainerTeamDetails
+              url={URL["royal-challengers-bangalore"]}
+            />
           </Route>
           <Route path="/teams/sunrisers-hyderabad" exact>
-            <CardContainer url={URL["sunrisers-hyderabad"]} />
-          </Route>
-          <Route>
-            <div>Not found</div>
+            <CardContainerTeamDetails url={URL["sunrisers-hyderabad"]} />
           </Route>
         </Switch>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,27 @@
 import React, { useCallback } from "react";
 import { useHistory } from "react-router-dom";
 
-import "./temp.css";
-
 function Card(props) {
   const { img, data } = props;
   const history = useHistory();
   const clickHandler = useCallback(() => {
     history.push(`teams/${data.id}`);
   }, [data, history]);
-  if (!data) {
-    return <h1>Loading...</h1>;
-  } else {
-    return (
-      <div className="card" onClick={clickHandler}>
-        <img src={img} alt="team-logo" />
+  return (
+    <div className="card" onClick={clickHandler}>
+      <img src={img} alt="team-logo" />
+      <div>
+        <h2>{data.teamName}</h2>
+        <h4>{data.venue}</h4>
         <div>
-          <h2>{data.teamName}</h2>
-          <h4>{data.venue}</h4>
-          <div>
-            <i className="fas fa-trophy"></i>
-            <span>{data.winningYears}</span>
-          </div>
+          <i className="fas fa-trophy"></i>
+          <span>{data.winningYears}</span>
         </div>
-        Team Home
-        <i className="fas fa-angle-right tab-arrow"></i>
       </div>
-    );
-  }
+      Team Home
+      <i className="fas fa-angle-right tab-arrow"></i>
+    </div>
+  );
 }
 
 export default Card;
