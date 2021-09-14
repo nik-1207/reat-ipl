e4c6cf9fe7ca77a4a997e99f64fa07f2a1a14c58
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-14 23:36:46 +0530

    routes optimized

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import Routes from "./Route/Routes";
+import Routes from "./Route/Routes.jsx";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <div className="App">
-        <Routes />
+        {Routes}
       </div>
     </Router>
   );
