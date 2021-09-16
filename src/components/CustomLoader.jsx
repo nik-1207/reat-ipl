dbf4aeefb7718e690b7fcab9c9ff95fff3c75b38
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-16 16:02:36 +0530

    adding pubsub

--- src/components/CustomLoader.jsx
+++ src/components/CustomLoader.jsx
@@ -4,24 +4,26 @@ import { LoadingEvent } from "../event/LoadingEvent";
 import { subscribe } from "../pubsub/PubSub";
 
 export default function CustomLoader() {
-const [loading, setloading] = useState()
+  const [loading, setloading] = useState();
 
   useEffect(() => {
-    const handle=subscribe(LoadingEvent,(args)=>
-    {
-
-      setloading(args.isLoading)
-    })
+    const handle = subscribe(LoadingEvent, (args) => {
+      setloading(args.isLoading);
+    });
     handle.unsubscribe();
   });
 
   return (
-    <>{loading && <Loader
-      style={{ margin: "25% 45%" }}
-      type="TailSpin"
-      color="#00BFFF"
-      height={80}
-      width={80}
-    />}</>
+    <>
+      {loading && (
+        <Loader
+          style={{ margin: "25% 45%" }}
+          type="TailSpin"
+          color="#00BFFF"
+          height={80}
+          width={80}
+        />
+      )}
+    </>
   );
 }
