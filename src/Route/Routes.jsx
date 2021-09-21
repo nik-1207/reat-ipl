8c21d8e35ac630d6044d4cca447a6b329b823f6b
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-21 10:32:38 +0530

    removing unnecessary import

--- src/Route/Routes.jsx
+++ src/Route/Routes.jsx
@@ -8,7 +8,16 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={AllTeams} />
       <Route path="/teams" exact component={AllTeams} />
-      <Route path={`/teams/:a`} exact component={TeamDetails} />
+      <Route path={`/teams/:url`} exact component={TeamDetails} />
+      {/* <Route path={`/teams/chennai-super-kings`} exact component={TeamDetails} />
+      <Route path={`/teams/delhi-capitals`} exact component={TeamDetails} />
+      <Route path={`/teams/kings-xi-punjab`} exact component={TeamDetails} />
+      <Route path={`/teams/kolkata-knight-riders`} exact component={TeamDetails} />
+      <Route path={`/teams/mumbai-indians`} exact component={TeamDetails} />
+      <Route path={`/teams/rajasthan-royals`} exact component={TeamDetails} />
+      <Route path={`/teams/royal-challengers-bangalore`} exact component={TeamDetails} />
+      <Route path={`/teams/sunrisers-hyderabad`} exact component={TeamDetails} /> */}
+
       <Route component={NotFound} />
     </Switch>
   );
